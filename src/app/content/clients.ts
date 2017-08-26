96126388f731fe22f7394a9e1b59ee7881f6423b
Hardik Patel <hardikgw@gmail.com>
2017-08-26 10:30:35 -0400

    clients

--- src/app/content/clients.ts
+++ src/app/content/clients.ts
@@ -1,38 +1,50 @@
 export const Clients = [
   {
-    'name' : 'USPTO',
-    'logo' : '../../assets/img/clients/uspto.png'
+    'name': 'USPTO',
+    'logo': '../../assets/img/clients/uspto.png'
   },
   {
-    'name' : 'PwC',
-    'logo' : '../../assets/img/clients/pwc.png'
+    'name': 'PwC',
+    'logo': '../../assets/img/clients/pwc.png'
   },
   {
-    'name' : 'FAA',
-    'logo' : '../../assets/img/clients/faa.png'
+    'name': 'FAA',
+    'logo': '../../assets/img/clients/faa.png'
   },
   {
-    'name' : 'McKinsey',
-    'logo' : '../../assets/img/clients/mck.jpg'
+    'name': 'McKinsey',
+    'logo': '../../assets/img/clients/mck.jpg'
   },
   {
-    'name' : 'Perficient',
-    'logo' : '../../assets/img/clients/perficient.png'
+    'name': 'Perficient',
+    'logo': '../../assets/img/clients/perficient.png'
   },
   {
-    'name' : 'NLRB',
-    'logo' : '../../assets/img/clients/nlrb.jpg'
+    'name': 'NLRB',
+    'logo': '../../assets/img/clients/nlrb.jpg'
   },
   {
-    'name' : 'DHS',
-    'logo' : '../../assets/img/clients/dhs.png'
+    'name': 'DHS',
+    'logo': '../../assets/img/clients/dhs.png'
   },
   {
-    'name' : 'SRA',
-    'logo' : '../../assets/img/clients/sra.gif'
+    'name': 'SRA',
+    'logo': '../../assets/img/clients/sra.gif'
   },
   {
-    'name' : 'TechMahindra',
-    'logo' : '../../assets/img/clients/techmahindra.jpg'
+    'name': 'TechMahindra',
+    'logo': '../../assets/img/clients/techmahindra.jpg'
   },
+  {
+    'name': 'NIH-NCI',
+    'logo': '../../assets/img/clients/nihnci.png'
+  },
+  {
+    'name': 'FannieMae',
+    'logo': '../../assets/img/clients/fanniemae.png'
+  },
+  {
+    'name': 'FraddieMac',
+    'logo': '../../assets/img/clients/fraddiemac.jpg'
+  }
 ];
