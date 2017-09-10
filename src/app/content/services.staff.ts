ad7ef3993dfbe2f5bbbb91095845ef1710bfd602
Hardik Patel <hardikgw@gmail.com>
2017-09-10 11:46:58 -0400

    staffing updates

--- src/app/content/services.staff.ts
+++ src/app/content/services.staff.ts
@@ -35,30 +35,8 @@ export const StaffingServices = {
       cols: "3",
       content: {
         title: "Diversity Recruiting",
-        subtitle: "We have successfully developed Big Data Enterprise Search systems for Federal and Commercial customers.",
-        body: [
-          {
-            title: "Document Security",
-            content: [
-              "Our team in vastly more familiar with the needs of our customers related to Authenticatioj, Authorization and Security of documents which resides in the system.",
-              "We help our customers to integrate Document Level Security in search indexes and search results by implementing stat of the art solutions which combines power of <strong>Graph DataAlgorithms and NOSQL datastores.</strong>"
-            ]
-          },
-          {
-            title: "Connectors",
-            content: [
-              "Our team has many years of experience building custom connectors that can access your data from all your sources including databases, intranets, network drives, SharePoint, Documentum, Google Drive, CRM systems and the cloud.",
-              "With the connector architecture we have built, our customers enjoy full control over source code, easy maintenance and version upgrades as they evolve/upgrade their content management platforms."
-            ]
-          },
-          {
-            title: "Data Ingestion",
-            content: [
-              "We have proven proficiency in optimizing Data Ingesting techniques in various Enterprise Search Platforms like Solr, Elastic, IBM Watson and Google Enterprise Search.",
-              "Our team is proficient in myriad of languages and tools which they use to provide Custom data ingestion process and gives full control to the customer."
-            ]
-          }
-        ]
+        subtitle: "",
+        body: []
       }
     },
     {
@@ -66,30 +44,8 @@ export const StaffingServices = {
       cols: "3",
       content: {
         title: "Proposal Recruiting",
-        subtitle: "We have successfully developed Big Data Enterprise Search systems for Federal and Commercial customers.",
-        body: [
-          {
-            title: "Document Security",
-            content: [
-              "Our team in vastly more familiar with the needs of our customers related to Authenticatioj, Authorization and Security of documents which resides in the system.",
-              "We help our customers to integrate Document Level Security in search indexes and search results by implementing stat of the art solutions which combines power of <strong>Graph DataAlgorithms and NOSQL datastores.</strong>"
-            ]
-          },
-          {
-            title: "Connectors",
-            content: [
-              "Our team has many years of experience building custom connectors that can access your data from all your sources including databases, intranets, network drives, SharePoint, Documentum, Google Drive, CRM systems and the cloud.",
-              "With the connector architecture we have built, our customers enjoy full control over source code, easy maintenance and version upgrades as they evolve/upgrade their content management platforms."
-            ]
-          },
-          {
-            title: "Data Ingestion",
-            content: [
-              "We have proven proficiency in optimizing Data Ingesting techniques in various Enterprise Search Platforms like Solr, Elastic, IBM Watson and Google Enterprise Search.",
-              "Our team is proficient in myriad of languages and tools which they use to provide Custom data ingestion process and gives full control to the customer."
-            ]
-          }
-        ]
+        subtitle: "",
+        body: []
       }
     }
   ]
