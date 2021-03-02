c92c1e3e473cdee1ef590e5a319f651f6b8ca54c
Hardik <hardikgw@gmail.com>
2021-03-02 05:40:51 -0500

    client grid final

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Clients} from '../content/clients';
 import {ActivatedRoute} from "@angular/router";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-clients',
@@ -8,13 +9,48 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
+  cols : number;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  gridByBreakpoint = {
+    xl: 5,
+    lg: 4,
+    md: 3,
+    sm: 2,
+    xs: 1
+  }
+
+  constructor(private activatedRoute: ActivatedRoute, private breakpointObserver: BreakpointObserver) {
     this.activatedRoute.fragment.subscribe(f => {
       const element = document.querySelector("#" + f);
       if (element) {
         element.scrollIntoView();
         window.scrollBy(0, -64);
+      }
+    });
+
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.cols = this.gridByBreakpoint.xs;
+        }
+        if (result.breakpoints[Breakpoints.Small]) {
+          this.cols = this.gridByBreakpoint.sm;
+        }
+        if (result.breakpoints[Breakpoints.Medium]) {
+          this.cols = this.gridByBreakpoint.md;
+        }
+        if (result.breakpoints[Breakpoints.Large]) {
+          this.cols = this.gridByBreakpoint.lg;
+        }
+        if (result.breakpoints[Breakpoints.XLarge]) {
+          this.cols = this.gridByBreakpoint.xl;
+        }
       }
     });
   }
