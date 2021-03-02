6ea25fa8da43a2b5d00f933af86d7fffe8319ea1
Hardik <hardikgw@gmail.com>
2021-03-02 03:54:57 -0500

    ng up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -50,7 +49,6 @@ import { ProductComponent } from './products/product/product.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     MatIconModule,
     MatMenuModule,
     MatListModule,
