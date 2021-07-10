79bba9a7f735959f457883bd036b59898c9f8480
Hardik <hardikgw@gmail.com>
2021-07-10 09:00:09 -0400

    stars

--- src/app/content/certs.ts
+++ src/app/content/certs.ts
@@ -1,15 +1,15 @@
 export const Certs = [
   {
+    'name': 'STARS III',
+    'logo': '/assets/img/certs/stars3.jpg'
+  },
+  {
     'name': '8a',
     'logo': '/assets/img/certs/8a.png'
   },
   {
     'name': 'EDWOSB',
     'logo': '/assets/img/certs/edwosb.png'
-  },
-  {
-    'name': 'STARS III',
-    'logo': '/assets/img/certs/stars3.jpg'
   },
   {
     'name': 'NMSDC',
