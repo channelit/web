f7e82658400d597cd866b7db2e58953410f8c35e
Hardik <hardikgw@gmail.com>
2021-07-11 21:49:17 -0400

    careers

--- src/app/content/careers.ts
+++ src/app/content/careers.ts
@@ -2,14 +2,54 @@ export const Careers = {
   title: "Current Job Openings",
   jobs: [
     {
-      title: "Experience with AWS technologies and DevSecOps",
-      subtitle: "Enterprise Search platform based on open source technologies, horizontally scalable, massively parallel processing system for streaming data analytics, machine learning, graph data and cognitive artificial intelligence.",
-      body: "Open source big data graph analytics platform."
+      id: "CIT-AWSSEC-034",
+      title: "AWS DevSecOps Engineer",
+      subtitle: "CITS is actively hiring for this position for our federal customer. Apply today ay by sending your resume",
+      responsibilities:
+        "<ul>" +
+        "<li>Build and maintain an AWS cloud infrastructure architecture aligning security, compliance, performance and resilience</li>" +
+        "<li>Provide expertise and best practices for implementing cloud security (internal) and product security (external)</li>" +
+        "<li>Assess architectures and designs for security vulnerabilities and suggest and implement proper alternatives</li>" +
+        "<li>Oversee the management and remediation of identified security flaws within our development platforms</li>" +
+        "<li>Build and maintain monitoring, auditing, and reporting frameworks that produces artifacts that support security and compliance needs</li>" +
+        "<li>Build and maintain a set of tools that enable developers to self-serve for most operational tasks</li>" +
+        "</ul>",
+      musthave:
+        "<ul>" +
+        "<li>Experience with cloud-based security management tools</li>" +
+        "<li>Programming or scripting experience with a popular modern languages (Java, Python, Ruby, etc.)</li>" +
+        "</ul>",
+      desired:
+        "<ul>" +
+        "<li>Strong interpersonal, oral, and written communication skills</li>" +
+        "<li>Ability to work in a fast-paced, rapidly changing, Agile, competitive environment</li>" +
+        "<li>Ability to work independently and in group environments</li>" +
+        "</ul>"
     },
     {
-      title: "Security Auditor",
-      subtitle: "An open source user interface for graph data analytics, and enterprise search.A System for the Creation of Knowledge Graph based on Social Network Data with Geographical and Temporal Information.",
-      body: "Security Auditor with CISSP certifications and governa",
+      id: "CIT-AWSSEC-035",
+      title: "Cloud Security Auditor",
+      subtitle: "CITS is actively hiring for this position for our federal customer. Apply today ay by sending your resume",
+      responsibilities:
+        "<ul>" +
+        "<li>Audit components of the business units including critical technology functions, cloud-based infrastructure, cybersecurity, risk management, application, and third-party management</li>" +
+        "<li>Design and execute internal control testing for operations of varying complexity. • Perform audit tasks of moderate difficulty, demonstrating a degree of audit expertise consistent with experience level</li>" +
+        "<li>Prepare clear, organized and complete documentation to support work performed in accordance with audit methodology and according to plan timelines</li>" +
+        "<li>Prioritize and effectively plan own work activities managing multiple priorities and tasks across the team to deliver quality results.</li>" +
+        "</ul>",
+      musthave:
+        "<ul>" +
+        "<li>Bachelor's degree in Finance, Accounting or Computer Science from four-year College or University</li>" +
+        "<li>Certifications related to Technology or Auditing, such as Cloud provider certifications, Certified Internal Auditor (CIA), Certified Information Systems Auditor (CISA), Certified Information Systems Security Professional (CISSP), or Certified Information</li>" +
+        "</ul>",
+      desired:
+        "<ul>" +
+        "<li>Ability to work with mathematical concepts such as probability and statistical inference, and fundamentals of plane and solid geometry and trigonometry</li>" +
+        "<li>Ability to apply concepts such as fractions, percentages, ratios, and proportions to practical situations</li>" +
+        "<li>Ability to carry out detailed written and/or verbal instructions</li>" +
+        "<li>Ability to solve problems involving concrete variables in standardized situations</li>" +
+        "<li>Ability to define problems, collect data, establish facts, and draw valid conclusions.</li>" +
+        "</ul>"
     }
   ]
 };
