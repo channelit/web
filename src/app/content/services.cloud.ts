d9fabc9f50e81b247a57309f6e16d41d58e02aa8
Hardik <hardikgw@gmail.com>
2021-07-14 10:26:17 -0400

    wcp

--- src/app/content/services.cloud.ts
+++ src/app/content/services.cloud.ts
@@ -4,28 +4,30 @@ export const ServicesCloud = {
     [
       {
         rows: "2",
-        cols: "3",
+        cols: "2",
         content: {
           title: "Cloud Migration",
           subtitle: "We have successfully developed <strong>Big Data Enterprise Search</strong> systems for Federal and Commercial customers.",
+          image: "assets/img/services/cloud/cloud_migration.png",
           body: [
             {
               title: "Discovery and Architecture",
               content: [
-                "MongoDB",
-                "Cassandra",
-                "Neo4J"
-              ]
+                "Methodically identify, plan and architect migration of legacy applications",
+                "Manage modernization across the enterprise using streamlined development integration and systematic information-sharing among teams",
+                "Migrate and automate infrastructure, security toolchain and applications to milti-cloud environments"
+              ],
             },
             {
               title: "Data Migration",
               content: [
-                "Hadoop",
-                "Spark",
-              ]
+                "Validation and compliance of data migration to safeguard sensitive data which includes testing and tooling",
+                "Continuously move large volumes of data to the cloud without disruption of production operations",
+                "Refactor or replatform migrated apps using test data."
+              ],
             }
-          ]
-        }
+          ],
+        },
       },
       {
         rows: "2",
